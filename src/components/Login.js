a89e7b066c50712ad2d6b2fe87bcc8a838227227
nickJ <nicknotjustin@gmail.com>
2019-02-04 11:25:03 -0600

    Login and Register now both log their respective values to the console

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,17 +6,21 @@ import {reduxForm, Field } from 'redux-form';
 //Import Validators later
 
 export class Login extends Component{
-
+    onSubmit(values) {
+        console.log(values);
+        //This will send a post request to the server to check username+password    
+        //Will send back a JWT token if correct
+    }
     render() {
     return(
         <div role="container" className="loginContainer">
-          <form>
+          <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
             <h5>I'm a login!</h5>
             <label htmlFor='userName'>UserName</label>
             <Field component={Input} type='text' name='UserName' element='input' />
             <label htmlFor='newPassword'>New Password</label>
             <Field component={Input} type='text' name='Password' element='input' />
-            <button>Confirm</button>
+            <button type='submit'>Confirm</button>
           </form>
         </div>
     )
--- src/components/Register.js
+++ src/components/Register.js
@@ -6,17 +6,21 @@ import {reduxForm, Field } from 'redux-form';
 //Import Validators later
 
 export class Register extends Component{
+    onSubmit(values) {
+        console.log(values);
+        //This will send a post request to the server to create a new user
+    }
 
     render() {
     return(
         <div role="container" className="registerContainer">
-          <form>
+          <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
             <h5>I'm a register!</h5>
             <label htmlFor='newUserName'>New UserName</label>
             <Field component={Input} type='text' name='newUserName' element='input' defaultValue='Type New UserName' />
             <label htmlFor='newPassword'>New Password</label>
             <Field component={Input} type='text' name='newPassword' element='input' defaultValue='Type New Password' />
-            <button>Confirm</button>
+            <button type='submit'>Confirm</button>
           </form>
         </div>
     )
